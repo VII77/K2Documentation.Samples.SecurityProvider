7e493d2a16278b283eb1a1e6a8d454ad00d419e3
VII77 <ulrichroessler@yahoo.com>
2023-03-10 21:04:45 +0100

    test 2

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using SampleSecurityProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var keycloakapi = new KeycloakApi("");
+            keycloakapi.GetUserById("asdf");
+        }
+    }
+}
