b65a7d2c7de7c93dffaa2c34de2650bda0f5d99c
VII77 <ulrichroessler@yahoo.com>
2023-03-21 14:34:06 +0100

    change props at group

--- KeycloakUserManager/API/KeycloakObjects/KeycloakGroup.cs
+++ KeycloakUserManager/API/KeycloakObjects/KeycloakGroup.cs
@@ -21,10 +21,11 @@ namespace KeycloakUserManager.API.KeycloakObjects
             {
                 this.Attributes = new Dictionary<string, object>()
             {
-                { "name", string.Empty },
-                { "email", string.Empty },
-                { "fullName", string.Empty },
-                { "description", string.Empty },
+                { "GroupID", this.id },
+                { "Name", this.name },
+                { "Email", string.Empty },
+                { "GroupName", this.name },
+                { "Description", string.Empty },
             };
             }
 
@@ -34,10 +35,11 @@ namespace KeycloakUserManager.API.KeycloakObjects
         {
             var dic = new Dictionary<string, object>()
             {
-                {"name", this.Attributes["name"] ?? this.name },
-                {"email", this.Attributes["email"] ?? string.Empty },
-                {"fullName", this.Attributes["fullName"] ?? string.Empty },
-                {"description", this.Attributes["description"] ?? string.Empty }
+                {"Name", this.Attributes["Name"] ?? this.name },
+                {"Email", this.Attributes["Email"] ?? string.Empty },
+                {"Description", this.Attributes["Description"] ?? string.Empty },
+                {"GroupName", this.Attributes["GroupName"] ?? string.Empty },
+                {"GroupID", this.Attributes["GroupID"] ?? string.Empty }
             };
 
             //< k:group name = "Test\G1" email = "G1@test.com" fullName = "Group 1" description = "Group 1 Description" >
